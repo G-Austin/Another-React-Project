77a0f93a86ba78b865391df42da0a930a3ffb396
Glo Austin <glo.austin19@gmail.com>
2019-01-31 14:46:58 -0800

    added axios interceptors

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -23,11 +23,12 @@ class FullPost extends Component {
   }
 
   deletePostHandler = () => {
-    axios.delete("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
-        .then(response => {
-            console.log(response)
-        })
-  }
+    axios
+      .delete("https://jsonplaceholder.typicode.com/posts/" + this.props.id)
+      .then(response => {
+        console.log(response);
+      });
+  };
 
   render() {
     let post = <p style={{ textAlign: "center" }}>Please select a Post!</p>;
@@ -40,10 +41,9 @@ class FullPost extends Component {
           <h1>{this.state.loadedPost.title}</h1>
           <p>{this.state.loadedPost.body}</p>
           <div className="Edit">
-            <button 
-                className="Delete"
-                onClick={this.deletePostHandler}
-            >Delete</button>
+            <button className="Delete" onClick={this.deletePostHandler}>
+              Delete
+            </button>
           </div>
         </div>
       );
